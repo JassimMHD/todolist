c25ba6b55cab4bd19dbedc8b7bd0f444e299ab6a
focaldive <focaldive@gmail.com>
2025-10-03 19:53:30 +0530

    Refactor TodoList component for improved readability and consistency in fetch handling

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -7,16 +7,26 @@ function TodoList() {
   const [editText, setEditText] = useState("");
   const [token, setToken] = useState(localStorage.getItem("token"));
 
+  // const token = localStorage.getItem("token");
   // Load todos
-  useEffect(function () {
-    if (!token) return;
-    fetch("http://localhost:5000/todos", {
-      headers: { Authorization: "Bearer " + token },
-    })
-      .then(function (res) { return res.json(); })
-      .then(function (data) { setTodos(data); })
-      .catch(function (err) { console.log(err); });
-  }, [token]);
+  useEffect(
+    function () {
+      if (!token) return;
+      fetch("http://localhost:5000/todos", {
+        headers: { Authorization: "Bearer " + token },
+      })
+        .then(function (res) {
+          return res.json();
+        })
+        .then(function (data) {
+          setTodos(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    },
+    [token]
+  );
 
   // Add todo
   function addTodo() {
@@ -28,12 +38,16 @@ function TodoList() {
       },
       body: JSON.stringify({ text: input }),
     })
-      .then(function (res) { return res.json(); })
+      .then(function (res) {
+        return res.json();
+      })
       .then(function (newTodo) {
         setTodos(todos.concat(newTodo));
         setInput("");
       })
-      .catch(function (err) { console.log(err); });
+      .catch(function (err) {
+        console.log(err);
+      });
   }
 
   // Delete todo
@@ -43,9 +57,15 @@ function TodoList() {
       headers: { Authorization: "Bearer " + token },
     })
       .then(function () {
-        setTodos(todos.filter(function (t) { return t._id !== id; }));
+        setTodos(
+          todos.filter(function (t) {
+            return t._id !== id;
+          })
+        );
       })
-      .catch(function (err) { console.log(err); });
+      .catch(function (err) {
+        console.log(err);
+      });
   }
 
   // Edit todo
@@ -63,13 +83,21 @@ function TodoList() {
       },
       body: JSON.stringify({ text: editText, completed: false }),
     })
-      .then(function (res) { return res.json(); })
+      .then(function (res) {
+        return res.json();
+      })
       .then(function (updated) {
-        setTodos(todos.map(function (t) { return t._id === updated._id ? updated : t; }));
+        setTodos(
+          todos.map(function (t) {
+            return t._id === updated._id ? updated : t;
+          })
+        );
         setEditId(null);
         setEditText("");
       })
-      .catch(function (err) { console.log(err); });
+      .catch(function (err) {
+        console.log(err);
+      });
   }
 
   function cancelEdit() {
@@ -85,11 +113,15 @@ function TodoList() {
         <input
           type="text"
           value={input}
-          onChange={function(e) { setInput(e.target.value); }}
+          onChange={function (e) {
+            setInput(e.target.value);
+          }}
           className="flex-1 px-3 py-2 border border-gray-600"
           placeholder="Enter new todo"
         />
-        <button onClick={addTodo} className="px-4 py-2 bg-gray-700 text-white">Add</button>
+        <button onClick={addTodo} className="px-4 py-2 bg-gray-700 text-white">
+          Add
+        </button>
       </div>
 
       <ul className="space-y-3">
@@ -101,11 +133,23 @@ function TodoList() {
                   <input
                     type="text"
                     value={editText}
-                    onChange={function(e) { setEditText(e.target.value); }}
+                    onChange={function (e) {
+                      setEditText(e.target.value);
+                    }}
                     className="flex-1 px-3 py-2 border border-gray-600"
                   />
-                  <button onClick={saveEdit} className="px-3 py-1 bg-blue-950 text-white">Save</button>
-                  <button onClick={cancelEdit} className="px-3 py-1 bg-gray-600 text-white">Cancel</button>
+                  <button
+                    onClick={saveEdit}
+                    className="px-3 py-1 bg-blue-950 text-white"
+                  >
+                    Save
+                  </button>
+                  <button
+                    onClick={cancelEdit}
+                    className="px-3 py-1 bg-gray-600 text-white"
+                  >
+                    Cancel
+                  </button>
                 </>
               ) : (
                 <>
@@ -115,8 +159,22 @@ function TodoList() {
                     disabled
                     className="flex-1 px-3 py-2 border border-gray-600 bg-gray-100 text-gray-700"
                   />
-                  <button onClick={function() { deleteTodo(todo._id); }} className="px-3 py-1 bg-teal-700 text-white">Delete</button>
-                  <button onClick={function() { startEdit(todo._id, todo.text); }} className="px-3 py-1 bg-teal-900 text-white">Edit</button>
+                  <button
+                    onClick={function () {
+                      deleteTodo(todo._id);
+                    }}
+                    className="px-3 py-1 bg-teal-700 text-white"
+                  >
+                    Delete
+                  </button>
+                  <button
+                    onClick={function () {
+                      startEdit(todo._id, todo.text);
+                    }}
+                    className="px-3 py-1 bg-teal-900 text-white"
+                  >
+                    Edit
+                  </button>
                 </>
               )}
             </div>
