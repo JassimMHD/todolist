d93a5bc570bb011ae00a884afdd0aae8211ce894
focaldive <focaldive@gmail.com>
2025-10-03 20:06:35 +0530

    Refactor server.js to use dbConn for MongoDB connection and update CORS origin to use environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
 import express from "express";
-import mongoose from "mongoose";
+
 import cors from "cors";
 import dotenv from "dotenv";
-import { authMiddleware } from "./middlewares/authMiddleware.js";
 import authRouter from "./controllers/authController.js";
 import todoRouter from "./controllers/todoController.js";
+import dbConn from "./config/dbConn.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-await mongoose.connect(process.env.MONGO_URI);
-console.log("MongoDB Connected");
+dbConn();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.URL, credentials: true }));
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/todos", todoRouter);
